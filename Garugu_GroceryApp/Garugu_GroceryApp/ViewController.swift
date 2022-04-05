ce2518c600c8c0698e2c7c005faaf9f6f4a6a3cf
sai shanmukh <garugusaishanmukh@gmail.com>
2022-04-05 14:33:43 -0500

    initial commit

---
+++ Garugu_GroceryApp/Garugu_GroceryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Garugu_GroceryApp
+//
+//  Created by Garugu,Sai Shanmukh on 4/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Garugu_GroceryApp/Garugu_GroceryAppUITests/Garugu_GroceryAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Garugu_GroceryAppUITestsLaunchTests.swift
+//  Garugu_GroceryAppUITests
+//
+//  Created by Garugu,Sai Shanmukh on 4/5/22.
+//
+
+import XCTest
+
+class Garugu_GroceryAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
